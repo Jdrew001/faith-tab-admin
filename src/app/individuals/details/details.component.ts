8d0275d012614db9c804d574e9e58712ba68ceb2
Drew Atkison <dtatkison@gmail.com>
2022-07-29 22:39:14 -0500

    family page

--- src/app/individuals/details/details.component.ts
+++ src/app/individuals/details/details.component.ts
@@ -7,6 +7,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
+  interactions = [
+    {
+      action: "Phone Call",
+      assignTo: "Jon Harris",
+      date: '02/23/2022'
+    },
+    {
+      action: "Text Message",
+      assignTo: "Jon Harris",
+      date: '02/23/2022'
+    }
+  ];
+  documents = [
+    {
+      documentName: 'Some Document',
+      uploadedBy: 'Jonathon Harris'
+    },
+    {
+      documentName: 'Some Document',
+      uploadedBy: 'Jonathon Harris'
+    },
+    {
+      documentName: 'Some Document',
+      uploadedBy: 'Jonathon Harris'
+    }
+  ];
+  familyMembers = [
+    {
+      firstName: 'Drew',
+      lastName: 'Atkison',
+      relationship: 'Main'
+    },
+    {
+      firstName: 'Elizabeth',
+      lastName: 'Atkison',
+      relationship: 'Spouse'
+    },
+    {
+      firstName: 'Noah',
+      lastName: 'Atkison',
+      relationship: 'Son'
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
